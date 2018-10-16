746a10015226e7ba5c6a3b1387b6a841e1352dd6
Ivan Blagajić <iblagajic@me.com>
2018-10-16 22:41:54 +0200

    Tidy up project structure (groups and folders)

---
+++ Vinyl/Services/Discogs+Keys.swift
@@ -0,0 +1,14 @@
+//
+//  Discogs+Keys.swift
+//  Vinyl
+//
+//  Created by Ivan Blagajic on 23/08/2018.
+//  Copyright © 2018 Five Dollar Milkshake Limited. All rights reserved.
+//
+
+import Foundation
+
+extension Discogs {
+    var key: String { return "LRQfscYPHHZliAPdKvQV" }
+    var secret: String { return "azrzoJGgUUTcKmQnhjvoIpbNjVJNWgPA" }
+}
